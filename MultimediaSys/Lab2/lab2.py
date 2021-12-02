2cafa5530e3c847bc5fdba533b29be1817160ba0
mjuc <michal.jucewicz@protonmail.com>
2021-12-02 22:12:47 +0100

    lab2,3

--- MultimediaSys/Lab2/lab2.py
+++ MultimediaSys/Lab2/lab2.py
@@ -1,12 +1,15 @@
+from matplotlib import image
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 import random
+from numpy.core.fromnumeric import shape
 from numpy.lib.function_base import copy
-
-IMAGES = ['0001.jpg','0002.jpg','0003.jpg','0004.png','0005.tif',
-        '0006.tif','0008.png','0009.png','0010.jpg','0011.jpg',
-        '0012.jpg','0013.jpg','0014.jpg','0015,jpg','0016.jpg']
+from progress.bar import Bar
+#'0001.jpg','0003.jpg',
+IMAGES = ['0009.png','0012.jpg']
+GRAY_BITS = [1,2,4]
+COLOR_BITS = [8,16]
 
 PALLETES = [np.array([[0, 0, 0],
 [0, 0, 1],
@@ -15,68 +18,159 @@ PALLETES = [np.array([[0, 0, 0],
 [1, 0, 0],
 [1, 0, 1],
 [1, 1, 0],
-[1, 1, 1]])]
+[1, 1, 1]]),
+np.array([
+[0, 0, 0],
+[0, 1, 1],
+[0, 0, 1],
+[1, 0, 1],
+[0, 0.5, 0],
+[0.5, 0.5, 0.5],
+[0, 1, 0],
+[0.5, 0, 0],
+[0, 0, 0.5],
+[0.5, 0.5, 0],
+[0.5, 0, 0.5],
+[1, 0, 0],
+[0.75, 0.75, 0.75],
+[0, 0.5, 0.5],
+[1, 1, 1],
+[1, 1, 0]
+]),
+np.array([[0.0745098039216,0.090196078,0.105882353],
+[0.0863,0.105882353,0.121568627],
+[0.094117647,0.121568627,0.121568627],
+[0.17254902,0.235294118,0.152941176],
+[0,0,0],
+[1,1,1],
+[0.184313725,0.239215686,0.156862745],
+[0.188235294,0.250980392,0.156862745],
+[0.203921569,0.254901961,0.168627451],
+[0.2,0.254901961,0.156862745],
+[0.219607843,0.270588235,0.17254902],
+[0.250980392,0.28627451,0.17254902]])]
 
-def readImage(imName):
-    img = cv2.imread(imName)
-    if (img.dtype == "uint8"):
-        img = np.float32(img/255)
-        return img
+def bitRed(img,n):
+    if img.dtype != float:
+        image = copy(img)/255
     else:
-        return img
+        image = copy(img)
 
-def colorFit(col,palette):
-    return palette[np.argmin(np.linalg.norm(col - palette,axis=1))]
-    
-def recolorImage(image,palette,n=1,Mpre=np.zeros((1,1))):
-    img = copy(image)
-    i = 0
-    for row in img:
-        j = 0
-        for pixel in row:
-            pixel=colorFit(pixel + Mpre[i % n,j % n],palette)
-            j+=1
-        i+=1
+    if len(img.shape) < 3:
+        palette = np.linspace(0, 1, 2 ** n)
+    else:
+        p = (n-8)%7
+        palette = PALLETES[p]
+    return recolorImage(image,palette)
+
+def recolorImage(image,palette):
+    img = image.copy()
+    if len(img.shape) < 3:
+        r,c = img.shape
+    else:
+        r,c, channels = img.shape
+    bar = Bar("Color fit",max=r*c)
+    for row in range(r):
+        for column in range(c):
+            img[row,column] = colorFit(img[row,column],palette)
+            bar.next()
+    bar.finish()
     return img
 
+def colorFit(col,palette):
+    res = palette[np.argmin(np.linalg.norm(col - palette,axis=0))]
+    return res
+    
 def randomDithering(image):
     img = copy(image)
+    r,c = img.shape
+    bar = Bar("Random ditheirng",max=r*c)
     for row in img:
         for pixel in row:
-            tmp = random()
+            tmp = random.random()
             if  pixel < tmp:
                 pixel = 0
             else:
                 pixel = 1
+            bar.next()
+    bar.finish()
     return img
 
-def organizedDithering(image):
+def organizedDithering(image,bit):
     M = np.array([[0, 8, 2, 10],
                   [12, 4, 14, 6],
                   [3, 11, 1, 9],
                   [15, 7, 13, 5]
                   ])
     n = M.shape[0]
+    img = copy(image)
+    if len(img.shape) < 3:
+        row,column = img.shape
+        palette = np.linspace(0, 1, 2 ** bit)
+    else:
+        row,column, channels = img.shape
+        p = (bit-8)%7
+        palette = PALLETES[p]
     Mpre = (M+1) / n ** 2 - 0.5
-    img = recolorImage(image,PALLETES[0],n,Mpre)
+    bar = Bar("Organized dithering",max=row*column)
+    for r in range(row):
+        for c in range(column):
+            c = colorFit(img[r,c] + Mpre[r % n, c % n],palette)
+            bar.next()
+    bar.finish()
     return img
 
-def floydSteinberg(image):
+def floydSteinberg(image,bit):
     img = copy(image)
-    row,column = img.shape
+    if len(img.shape) < 3:
+        row,column = img.shape
+        palette = np.linspace(0, 1, 2 ** bit)
+    else:
+        row,column, channels = img.shape
+        p = (bit-8)%7
+        palette = PALLETES[p]
+    bar = Bar('Floyd-Steinberg',max=row*column)
     for r in range(row):
         for c in range(column):
             oldpixel = copy(img[r,c])
-            newpixel  = colorFit(img[r,c], PALLETES[0])
+            newpixel  = colorFit(img[r,c], PALLETES[1])
             quantErr = oldpixel - newpixel
             if r < row - 1:
-                    img[r + 1][c] = img[r + 1][c].copy() + quantErr * 7 / 16
+                    img[r + 1][c] = img[r + 1][c] + (quantErr * (7 / 16))
             if r > 0 and c < column - 1:
-                img[r - 1][c + 1] = img[r - 1][c + 1].copy() + quantErr * 3 / 16
+                img[r - 1][c + 1] = img[r - 1][c + 1] + (quantErr * (3 / 16))
             if c < column - 1:
-                img[r][c + 1] = img[r][c + 1].copy() + quantErr * 5 / 16
+                img[r][c + 1] = img[r][c + 1] + (quantErr * (5 / 16))
             if r < row - 1 and c < column - 1:
-                img[r + 1][c + 1] = img[r + 1][c + 1].copy() + quantErr * 1 / 16
+                img[r + 1][c + 1] = img[r + 1][c + 1] + (quantErr * (1 / 16))
+            bar.next()
+    bar.finish()
+    return img
 
-img = readImage(IMAGES[3])
-#recolorImage(img,PALLETES[0])
+i = 2
+for im in IMAGES:
+    print(im)
+    if i < 2:
+        image = cv2.cvtColor(cv2.imread(im),cv2.COLOR_BGR2RGB)
+        j = 0
+        for bit in COLOR_BITS:
+            print(bit," bits")
+            tmp = bitRed(image,bit)
+            plt.imsave("{}{}bit.jpg".format(im,bit),tmp)
+            plt.imsave("orgdith_{}_{}.jpg".format(im,bit),organizedDithering(tmp,bit))
+            plt.imsave("fsdith_{}_{}.jpg".format(im,bit),floydSteinberg(tmp,bit))
+            j+=1
+    else:
+        image = cv2.cvtColor(cv2.imread(im),cv2.COLOR_BGR2GRAY)
+        for bit in GRAY_BITS:
+            print(bit," bit(s)")
+            tmp = bitRed(image,bit)
+            plt.imsave("{}{}bit.jpg".format(im,bit),tmp)
+            plt.imsave("randdith_{}.jpg".format(im),randomDithering(tmp))
+            plt.imsave("orgdith_{}_{}.jpg".format(im,bit),organizedDithering(tmp,bit))
+            plt.imsave("fsdith_{}_{}.jpg".format(im,bit),floydSteinberg(tmp,bit))
+    i+=1
+for bit in COLOR_BITS:
+    image = cv2.cvtColor(cv2.imread('0002.jpg'),cv2.COLOR_BGR2RGB)
+    tmp = bitRed(image,bit)
+    plt.imsave("z3{}bits.jpg".format(bit),recolorImage(tmp,PALLETES[2]))
--- MultimediaSys/Lab3/l3.py
+++ MultimediaSys/Lab3/l3.py
@@ -13,7 +13,7 @@ def bitRed(snd,n):
     mx = np.iinfo(oldtype).max
 
     if snd.dtype != float:
-        sound = float(copy(snd))
+        sound = copy(snd.astype(float))
     else:
         sound = copy(snd)
     
@@ -27,12 +27,12 @@ def bitRed(snd,n):
 def dM(snd,n,Fs):
     nFs = Fs/n
     sound = snd[0:snd.size - 1:n]
-    return nFs,sound
+    return int(nFs),sound
 
 def interp(snd,Fs,Fs2):
     T = (snd.size - 1)/Fs
-    t1 = np.linspace(0,T,snd.size)
-    xt = (Fs2 * T) + 1
+    t1 = np.linspace(0,T,len(snd)-1)
+    xt = int((Fs2 * T) + 1)
     t2 = np.linspace(0,T,xt)
 
     intr = interp1d(t1,snd)
@@ -40,3 +40,64 @@ def interp(snd,Fs,Fs2):
 
     res_lin = intr(t2)
     res_nonlin = intr_nonlin(t2)
+    return (res_lin,res_nonlin)
+
+BITS = [4,8,16,24]
+FREQS = [2000,4000,8000,16000,24000,41000,16950]
+FILES = ['sin_60Hz.wav','sin_440Hz.wav','sin_8000Hz.wav','sin_combined.wav','sing_high1.wav','sing_high2.wav','sing_low1.wav','sing_low2.wav','sing_medium1.wav','sing_medium2.wav']
+
+# i = 0
+# for file in FILES:
+#     sound, fs = sf.read(file, dtype=np.int32)
+#     if i < 4:
+#         for bit in BITS:
+#             sf.write("{}_{}bit.wav".format(file,bit),bitRed(sound, bit),fs)
+#         for fr in FREQS:
+#             ns, nl = interp(sound,fs,fr)
+#             sf.write("lin{}_{}hz.wav".format(file,fr),ns,fr)
+#             sf.write("nonlin{}_{}hz.wav".format(file,fr),nl,fr)
+#     else:
+#         for bit in BITS:
+#             sf.write("{}_{}bit.wav".format(file,bit),bitRed(sound, bit),fs)
+#         for fr in FREQS:
+#             if fr != 16950:
+#                 ns, nl = interp(sound,fs,fr)
+#                 sf.write("lin{}_{}hz.wav".format(file,fr),ns,fr)
+#                 sf.write("nonlin{}_{}hz.wav".format(file,fr),nl,fr)
+#     i+=1
+
+# for file in FILES:
+#     sound, fs = sf.read(file,dtype = np.int32)
+#     nfs, res = dM(sound,4,fs)
+#     sf.write("{}_dec.wav".format(file),res,nfs)
+
+# PLOT_FILES = ['sin_60Hz.wav','sin_440Hz.wav','sin_8000Hz.wav','sin_combined.wav']
+# for file in PLOT_FILES:
+#     t = 500
+#     for bit in BITS:
+#         sound , fs = sf.read('{}_{}bit.wav'.format(file,bit))
+#         tmp = []
+#         for i in range(t):
+#             tmp.append(sound[i])
+#         plt.plot(np.linspace(0,t,t),tmp)
+#         plt.savefig("{}_{}bit_plot.jpg".format(file,bit))
+#     for freq in FREQS:
+#         lins, a = sf.read("lin{}_{}hz.wav".format(file,freq))
+#         nonlins, b = sf.read("nonlin{}_{}hz.wav".format(file,freq))
+#         lintmp = []
+#         nonlintmp = []
+#         for i in range(t):
+#             lintmp.append(lins[i])
+#             nonlintmp.append(nonlins[i])
+#         plt.plot(np.linspace(0,t,t),lintmp)
+#         plt.savefig("lin{}_{}hz_plot.jpg".format(file,freq))
+#         plt.plot(np.linspace(0,t,t),nonlintmp)
+#         plt.savefig("nonlin{}_{}hz_plot.jpg".format(file,freq))
+
+sound, fs = sf.read("sin_60Hz.wav_dec.wav")
+t = 500
+tmp = []
+for i in range(t):
+    tmp.append(sound[i])
+plt.plot(np.linspace(0,t,t),tmp)
+plt.savefig("sin_60Hz.wav_dec.wav_plot.jpg")
